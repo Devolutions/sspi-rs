db7e20fbf03aeca294d5b8ac9400c96d60a3d4e1
Marc-André Moreau <marcandre.moreau@gmail.com>
2019-05-31 10:50:20 -0400

    sspi-rs: import CredSSP from Apriorit deliverable

--- src/encryption.rs
+++ src/encryption.rs
@@ -6,8 +6,10 @@ use crypto_mac::Mac;
 use hmac;
 use md4::{Digest, Md4};
 use md5::Md5;
+use sha2::Sha256;
 
 pub const HASH_SIZE: usize = 16;
+pub const SHA256_SIZE: usize = 32;
 
 pub fn compute_md4(data: &[u8]) -> [u8; HASH_SIZE] {
     let mut context = Md4::new();
@@ -21,6 +23,15 @@ pub fn compute_md4(data: &[u8]) -> [u8; HASH_SIZE] {
 pub fn compute_md5(data: &[u8]) -> [u8; HASH_SIZE] {
     let mut context = Md5::new();
     let mut result = [0x00; HASH_SIZE];
+    context.input(data);
+    result.clone_from_slice(&context.result());
+
+    result
+}
+
+pub fn compute_sha256(data: &[u8]) -> [u8; SHA256_SIZE] {
+    let mut context = Sha256::new();
+    let mut result = [0x00; SHA256_SIZE];
     context.input(data);
     result.clone_from_slice(&context.result());
 
--- src/lib.rs
+++ src/lib.rs
@@ -2,10 +2,16 @@
 // https://github.com/rust-num/num-derive/issues/20 is fixed
 #![cfg_attr(feature = "cargo-clippy", allow(clippy::useless_attribute))]
 
+mod ber;
 mod encryption;
+
+pub mod credssp;
 pub mod ntlm;
 pub mod sspi;
 
+pub use crate::credssp::{
+    ts_request::TsRequest, CredSsp, CredSspClient, CredSspResult, CredSspServer, CredentialsProxy,
+};
 pub use crate::ntlm::NTLM_VERSION_SIZE;
-pub use crate::sspi::{Credentials, SspiError, SspiErrorType};
+pub use crate::sspi::{Credentials, Sspi, SspiResult, SspiError, SspiErrorType};
 
