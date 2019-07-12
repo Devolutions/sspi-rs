12334d66999067e536ecc30297d9c0f75a5c8ed8
Vlad Alex <vlad.al.dp@gmail.com>
2019-07-12 11:14:52 +0300

    sspi-rs: move common functions to utils module

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,10 @@
+//! The Rust implementation of the Remote Desktop Protocol.
+
 // FromPrimitive and ToPrimitive causes clippy error, so we disable it until
 // https://github.com/rust-num/num-derive/issues/20 is fixed
 #![cfg_attr(feature = "cargo-clippy", allow(clippy::useless_attribute))]
 
+mod utils;
 mod ber;
 mod crypto;
 
