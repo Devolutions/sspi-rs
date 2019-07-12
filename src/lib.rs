ea5a99fcb5d27ae85465ce68eeff1cf326ff4452
Vlad Alex <vlad.al.dp@gmail.com>
2019-07-12 11:28:31 +0300

    sspi-rs: minor refactoring of lib.rs file

--- src/lib.rs
+++ src/lib.rs
@@ -1,20 +1,21 @@
-//! The Rust implementation of the Remote Desktop Protocol.
-
 // FromPrimitive and ToPrimitive causes clippy error, so we disable it until
 // https://github.com/rust-num/num-derive/issues/20 is fixed
 #![cfg_attr(feature = "cargo-clippy", allow(clippy::useless_attribute))]
 
-mod utils;
-mod ber;
-mod crypto;
-
+#[macro_use]
+pub mod utils;
+pub mod ber;
 pub mod credssp;
 pub mod ntlm;
 pub mod sspi;
 
-pub use crate::credssp::{
-    ts_request::TsRequest, CredSsp, CredSspClient, CredSspResult, CredSspServer, CredentialsProxy,
-};
-pub use crate::ntlm::NTLM_VERSION_SIZE;
-pub use crate::sspi::{Credentials, Sspi, SspiResult, SspiError, SspiErrorType};
+mod crypto;
 
+pub use crate::{
+    credssp::{
+        ts_request::TsRequest, CredSsp, CredSspClient, CredSspResult, CredSspServer, CredentialsProxy,
+        EarlyUserAuthResult, EARLY_USER_AUTH_RESULT_PDU_SIZE, NegotiationRequestFlags
+    },
+    ntlm::NTLM_VERSION_SIZE,
+    sspi::{Credentials, SspiError, SspiErrorType},
+};
