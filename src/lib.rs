f30f1a4738d587959b9db1ebf38e92f347bebb3a
Vlad Alex <vlad.al.dp@gmail.com>
2019-07-12 13:06:57 +0300

    sspi-rs: remove clippy attribute from num_traits import

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,3 @@
-// FromPrimitive and ToPrimitive causes clippy error, so we disable it until
-// https://github.com/rust-num/num-derive/issues/20 is fixed
-#![cfg_attr(feature = "cargo-clippy", allow(clippy::useless_attribute))]
-
 #[macro_use]
 pub mod utils;
 pub mod ber;
