b7394b5f2b1bb20f9481273e36ca26b3af58b63a
Benoît CORTIER <benoit.cortier@fried-world.eu>
2021-08-26 12:10:28 +0200

    Update crypto-related dependencies
    - md-5 to 0.9
    - md4 to 0.9
    - sha2 to 0.9
    - hmac to 0.11
    - crypto-mac to 0.11

--- src/crypto.rs
+++ src/crypto.rs
@@ -17,8 +17,8 @@ const SHA256_SIZE: usize = 32;
 pub fn compute_md4(data: &[u8]) -> [u8; HASH_SIZE] {
     let mut context = Md4::new();
     let mut result = [0x00; HASH_SIZE];
-    context.input(data);
-    result.clone_from_slice(&context.result());
+    context.update(data);
+    result.clone_from_slice(&context.finalize());
 
     result
 }
@@ -26,8 +26,8 @@ pub fn compute_md4(data: &[u8]) -> [u8; HASH_SIZE] {
 pub fn compute_md5(data: &[u8]) -> [u8; HASH_SIZE] {
     let mut context = Md5::new();
     let mut result = [0x00; HASH_SIZE];
-    context.input(data);
-    result.clone_from_slice(&context.result());
+    context.update(data);
+    result.clone_from_slice(&context.finalize());
 
     result
 }
@@ -35,22 +35,23 @@ pub fn compute_md5(data: &[u8]) -> [u8; HASH_SIZE] {
 pub fn compute_sha256(data: &[u8]) -> [u8; SHA256_SIZE] {
     let mut context = Sha256::new();
     let mut result = [0x00; SHA256_SIZE];
-    context.input(data);
-    result.clone_from_slice(&context.result());
+    context.update(data);
+    result.clone_from_slice(&context.finalize());
 
     result
 }
 
 pub fn compute_hmac_md5(key: &[u8], input: &[u8]) -> io::Result<[u8; HASH_SIZE]> {
-    let mut mac = hmac::Hmac::<Md5>::new_varkey(key).map_err(|e| {
+    use hmac::NewMac;
+    let mut mac = hmac::Hmac::<Md5>::new_from_slice(key).map_err(|e| {
         io::Error::new(
             io::ErrorKind::Other,
             format!("Failed to compute hmac md5: {}", e),
         )
     })?;
     let mut result = [0x00; HASH_SIZE];
-    mac.input(input);
-    result.clone_from_slice(&mac.result().code());
+    mac.update(input);
+    result.clone_from_slice(&mac.finalize().into_bytes());
 
     Ok(result)
 }
