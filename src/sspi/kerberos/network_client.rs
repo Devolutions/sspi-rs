973762bd08a32c824bfa85c37d49d1cb907d2388
Alexandr Yusuk <aleksandr.yusuk@apriorit.com>
2022-05-12 21:04:22 +0300

    kerberos: rename feature with_request_http_client -> with_reqwest_network_client. small .gitignore cleanup.

--- src/sspi/kerberos/network_client.rs
+++ src/sspi/kerberos/network_client.rs
@@ -10,7 +10,7 @@ pub trait NetworkClient: Debug {
     fn clone(&self) -> Box<dyn NetworkClient>;
 }
 
-#[cfg(feature = "with_reqwest_network_client")]
+#[cfg(feature = "network_client")]
 pub mod reqwest_network_client {
     use std::{
         io::{Read, Write},
