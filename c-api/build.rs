fc88bb16f9038bd9b8aac13daf63aa0688df7334
Marc-André Moreau <mamoreau@devolutions.net>
2022-05-30 15:06:37 -0400

    use /FORCE:MULTIPLE to workaround symbol conflict on Windows

---
+++ c-api/build.rs
@@ -0,0 +1,9 @@
+use std::env;
+
+fn main() {
+    let target_os = env::var("CARGO_CFG_TARGET_OS").unwrap();
+
+    if target_os == "windows" {
+        println!("cargo:rustc-link-arg=/FORCE:MULTIPLE");
+    }
+}
