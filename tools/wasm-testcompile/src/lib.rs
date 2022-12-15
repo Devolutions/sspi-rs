ee636110eb85b10eb4e609ad49c6adc52d86f71c
Benoît CORTIER <bcortier@proton.me>
2022-12-15 18:22:34 -0500

    ci: check sspi crate is WASM-compatible

---
+++ tools/wasm-testcompile/src/lib.rs
@@ -0,0 +1,21 @@
+use sspi::internal::credssp;
+use sspi::ntlm::AuthIdentity;
+use wasm_bindgen::prelude::*;
+
+#[wasm_bindgen]
+pub fn credssp_client() {
+    let mut cred_ssp_client = credssp::CredSspClient::new(
+        Vec::new(),
+        AuthIdentity::default(),
+        credssp::CredSspMode::WithCredentials,
+        credssp::ClientMode::Negotiate(sspi::NegotiateConfig::default()),
+        String::new(),
+    )
+    .unwrap();
+
+    let next_ts_request = credssp::TsRequest::default();
+
+    let result = cred_ssp_client.process(next_ts_request).unwrap();
+
+    std::hint::black_box(result);
+}
