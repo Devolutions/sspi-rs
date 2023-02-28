42b0b99d31f543e030303daf1ab8f9a3aed5d204
Alex Yusuk <aleksandr.yusuk@apriorit.com>
2023-02-28 23:50:24 +0200

    ffi: use .as_mut_ptr() instead of .as_ptr() in ChangePasswordW

--- ffi/src/sec_handle.rs
+++ ffi/src/sec_handle.rs
@@ -1047,19 +1047,19 @@ pub unsafe extern "system" fn ChangeAccountPasswordW(
         check_null!(psz_new_password);
         check_null!(p_output);
 
-        let security_package_name = c_w_str_to_string(psz_package_name);
+        let mut security_package_name = c_w_str_to_string(psz_package_name);
 
-        let domain = c_w_str_to_string(psz_domain_name);
-        let username = c_w_str_to_string(psz_account_name);
+        let mut domain = c_w_str_to_string(psz_domain_name);
+        let mut username = c_w_str_to_string(psz_account_name);
         let mut password = Secret::new(c_w_str_to_string(psz_old_password));
         let mut new_password = Secret::new(c_w_str_to_string(psz_new_password));
 
         ChangeAccountPasswordA(
-            security_package_name.as_ptr() as *mut _,
-            domain.as_ptr() as *mut _,
-            username.as_ptr() as *mut _,
-            password.as_mut().as_ptr() as *mut _,
-            new_password.as_mut().as_ptr() as *mut _,
+            security_package_name.as_mut_ptr() as *mut _,
+            domain.as_mut_ptr() as *mut _,
+            username.as_mut_ptr() as *mut _,
+            password.as_mut().as_mut_ptr() as *mut _,
+            new_password.as_mut().as_mut_ptr() as *mut _,
             b_impersonating,
             dw_reserved,
             p_output,
