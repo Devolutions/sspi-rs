02fe831597b6f514c80175d9cba8d5ffcb455160
Vladyslav Nikonov <mail@pacmancoder.xyz>
2023-07-21 16:49:47 +0300

    Fixed lints

--- ffi/src/sec_handle.rs
+++ ffi/src/sec_handle.rs
@@ -92,7 +92,7 @@ fn create_negotiate_context(attributes: &CredentialsAttributes) -> Result<Negoti
 
     if let Some(kdc_url) = attributes.kdc_url() {
         let kerberos_config =
-            KerberosConfig::new(&kdc_url, Box::new(ReqwestNetworkClient::default()), hostname.clone());
+            KerberosConfig::new(&kdc_url, Box::<ReqwestNetworkClient>::default(), hostname.clone());
         let negotiate_config = NegotiateConfig::new(
             Box::new(kerberos_config),
             attributes.package_list.clone(),
@@ -155,7 +155,7 @@ pub(crate) unsafe fn p_ctxt_handle_to_sspi_context(
                 if let Some(kdc_url) = attributes.kdc_url() {
                     SspiContext::Kerberos(Kerberos::new_client_from_config(KerberosConfig::new(
                         &kdc_url,
-                        Box::new(ReqwestNetworkClient::default()),
+                        Box::<ReqwestNetworkClient>::default(),
                         hostname,
                     ))?)
                 } else {
