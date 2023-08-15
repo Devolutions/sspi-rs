3b086f4772aaa759202212dc451ba935b0e49831
Pavlo Myroniuk <pavlo.myroniuk@apriorit.com>
2023-08-15 19:19:45 +0300

    feat(sspi): cert_utils: code and tests refactoring. add more comments. remove unneeded logs;

--- src/cert_utils.rs
+++ src/cert_utils.rs
@@ -10,12 +10,18 @@ use winapi::ctypes::c_void;
 use winapi::um::ncrypt::HCRYPTKEY;
 use winapi::um::wincrypt::{
     CertCloseStore, CertEnumCertificatesInStore, CertFreeCertificateContext, CertOpenStore, CryptAcquireContextW,
-    CryptGetKeyParam, CryptGetProvParam, CryptGetUserKey, AT_KEYEXCHANGE, CERT_STORE_PROV_SYSTEM_W,
-    CERT_SYSTEM_STORE_CURRENT_USER_ID, CERT_SYSTEM_STORE_LOCATION_SHIFT, CRYPT_FIRST, CRYPT_NEXT, CRYPT_SILENT,
-    HCRYPTPROV, KP_CERTIFICATE, PP_ENUMCONTAINERS, PP_SMARTCARD_READER, PROV_RSA_FULL,
+    CryptDestroyKey, CryptGetKeyParam, CryptGetProvParam, CryptGetUserKey, CryptReleaseContext, AT_KEYEXCHANGE,
+    CERT_STORE_PROV_SYSTEM_W, CERT_SYSTEM_STORE_CURRENT_USER_ID, CERT_SYSTEM_STORE_LOCATION_SHIFT, CRYPT_FIRST,
+    CRYPT_NEXT, CRYPT_SILENT, HCRYPTPROV, KP_CERTIFICATE, PP_ENUMCONTAINERS, PP_SMARTCARD_READER, PROV_RSA_FULL,
 };
 
-const CSP_NAME: &str = "Microsoft Base Smart Card Crypto Provider\0";
+// UTF-16 encoded "Microsoft Base Smart Card Crypto Provider\0"
+const CSP_NAME_W: &[u8] = &[
+    77, 0, 105, 0, 99, 0, 114, 0, 111, 0, 115, 0, 111, 0, 102, 0, 116, 0, 32, 0, 66, 0, 97, 0, 115, 0, 101, 0, 32, 0,
+    83, 0, 109, 0, 97, 0, 114, 0, 116, 0, 32, 0, 67, 0, 97, 0, 114, 0, 100, 0, 32, 0, 67, 0, 114, 0, 121, 0, 112, 0,
+    116, 0, 111, 0, 32, 0, 80, 0, 114, 0, 111, 0, 118, 0, 105, 0, 100, 0, 101, 0, 114, 0, 0, 0,
+];
+const CSP_NAME: &str = "Microsoft Base Smart Card Crypto Provider";
 
 use crate::{Error, ErrorKind, Result};
 
@@ -87,15 +93,10 @@ pub unsafe fn extract_certificate_by_thumbprint(thumbprint: &[u8]) -> Result<(Ve
 }
 
 #[instrument(level = "trace", ret)]
-unsafe fn acquire_context(key_container_name: &str) -> Result<HCRYPTPROV> {
-    let mut container_name = key_container_name
+unsafe fn acquire_key_container_context(key_container_name: &str) -> Result<HCRYPTPROV> {
+    let container_name = key_container_name
         .encode_utf16()
-        .flat_map(|v| v.to_le_bytes())
-        .collect::<Vec<_>>();
-    // add wide null char
-    container_name.extend_from_slice(&[0, 0]);
-    let csp_name = CSP_NAME
-        .encode_utf16()
+        .chain(std::iter::once(0))
         .flat_map(|v| v.to_le_bytes())
         .collect::<Vec<_>>();
     let mut crypt_context_handle = HCRYPTPROV::default();
@@ -103,7 +104,7 @@ unsafe fn acquire_context(key_container_name: &str) -> Result<HCRYPTPROV> {
     if CryptAcquireContextW(
         &mut crypt_context_handle,
         container_name.as_ptr() as *const _,
-        csp_name.as_ptr() as *const _,
+        CSP_NAME_W.as_ptr() as *const _,
         PROV_RSA_FULL,
         CRYPT_SILENT,
     ) == 0
@@ -160,13 +161,17 @@ pub unsafe fn get_key_container_certificate(crypt_context_handle: HCRYPTPROV) ->
 
     let mut cert_data_len = 0;
     if CryptGetKeyParam(key, KP_CERTIFICATE, null_mut(), &mut cert_data_len, 0) == 0 {
+        CryptDestroyKey(key);
         return Err(Error::new(ErrorKind::InternalError, "Cannot get certificate data len."));
     }
 
     let mut cert_data = vec![0; cert_data_len as usize];
     if CryptGetKeyParam(key, KP_CERTIFICATE, cert_data.as_mut_ptr(), &mut cert_data_len, 0) == 0 {
+        CryptDestroyKey(key);
         return Err(Error::new(ErrorKind::InternalError, "Cannot get certificate data."));
     }
+
+    CryptDestroyKey(key);
 
     Ok(picky_asn1_der::from_bytes(&cert_data)?)
 }
@@ -180,18 +185,18 @@ pub struct SmartCardInfo {
     pub private_key_file_index: u8,
 }
 
+// This function gathers the smart card information like reader name, key container name,
+// and so on using the provided certificate serial number.
+// It iterates over existing key containers and tries to find a suitable reader name and key container.
+// The similar approach is implemented in the FreeRDP for the smart card information gathering:
+// https://github.com/FreeRDP/FreeRDP/blob/56324906a2d5b2538675e2f10b9f1ffe4a27de79/libfreerdp/core/smartcardlogon.c#L616
 #[instrument(level = "trace", ret)]
 pub unsafe fn finalize_smart_card_info(cert_serial_number: &[u8]) -> Result<SmartCardInfo> {
-    let csp_name = CSP_NAME
-        .encode_utf16()
-        .flat_map(|v| v.to_le_bytes())
-        .collect::<Vec<_>>();
-
     let mut crypt_context_handle = HCRYPTPROV::default();
     if CryptAcquireContextW(
         &mut crypt_context_handle,
         null(),
-        csp_name.as_ptr() as *const _,
+        CSP_NAME_W.as_ptr() as *const _,
         PROV_RSA_FULL,
         CRYPT_SILENT,
     ) == 0
@@ -229,16 +234,11 @@ pub unsafe fn finalize_smart_card_info(cert_serial_number: &[u8]) -> Result<Smar
         {
             break;
         }
-        println!(
-            "key container name: {:?} {:?}",
-            String::from_utf8(key_container_name.clone()),
-            key_container_name
-        );
         let mut key_container_name = String::from_utf8(key_container_name).unwrap();
         // remove null char
         key_container_name.pop();
 
-        let context = if let Ok(context) = acquire_context(&key_container_name) {
+        let context = if let Ok(context) = acquire_key_container_context(&key_container_name) {
             context
         } else {
             index += 1;
@@ -247,7 +247,11 @@ pub unsafe fn finalize_smart_card_info(cert_serial_number: &[u8]) -> Result<Smar
 
         if let Ok(certificate) = get_key_container_certificate(context) {
             if certificate.tbs_certificate.serial_number.0 == cert_serial_number {
-                let reader_name = match get_reader_name(crypt_context_handle) {
+                let reader_name = get_reader_name(crypt_context_handle);
+
+                CryptReleaseContext(crypt_context_handle, 0);
+
+                let reader_name = match reader_name {
                     Ok(reader_name) => reader_name,
                     Err(err) => {
                         error!(?err);
@@ -255,19 +259,17 @@ pub unsafe fn finalize_smart_card_info(cert_serial_number: &[u8]) -> Result<Smar
                     }
                 };
 
-                let mut csp_name = CSP_NAME.to_owned();
-                // remove null byte
-                csp_name.pop();
-
                 return Ok(SmartCardInfo {
                     key_container_name,
                     reader_name,
                     certificate,
-                    csp_name,
+                    csp_name: CSP_NAME.to_owned(),
                     private_key_file_index: index,
                 });
             }
         }
+
+        CryptReleaseContext(crypt_context_handle, 0);
 
         index += 1;
         is_first = false;
@@ -276,8 +278,9 @@ pub unsafe fn finalize_smart_card_info(cert_serial_number: &[u8]) -> Result<Smar
     Err(Error::new(ErrorKind::InternalError, "Cannot get smart card info"))
 }
 
+// This function tries to extract the user principal name from the smart card certificate by searching in the Subject Alternative Name.
 #[instrument(level = "trace", ret)]
-pub fn extract_user_name_from_alt_name(certificate: &Certificate) -> Result<String> {
+pub fn extract_upn_from_alt_name(certificate: &Certificate) -> Result<String> {
     let subject_alt_name_ext = &certificate
         .tbs_certificate
         .extensions
@@ -296,7 +299,7 @@ pub fn extract_user_name_from_alt_name(certificate: &Certificate) -> Result<Stri
     let alternate_name = match subject_alt_name_ext {
         ExtensionView::SubjectAltName(alternate_name) => alternate_name,
         // safe: checked above
-        _ => unreachable!(),
+        _ => unreachable!("ExtensionView must be SubjectAltName"),
     };
     let other_name = match alternate_name.0.get(0).unwrap() {
         GeneralName::OtherName(other_name) => other_name,
@@ -319,6 +322,15 @@ pub fn extract_user_name_from_alt_name(certificate: &Certificate) -> Result<Stri
     Ok(data.to_string())
 }
 
+// This function tries to extract the user name from the smart card certificate
+// by extracting and concatenating the subject name fields of the provided certificate.
+// Usually, the smart card certificate subject name has the following structure: [domain components], "Users", <username component>.
+// For instance:
+// DC = com
+// DC = example
+// CN = Users
+// CN = pw11
+// From the example above, this function will return "pw13@example.com".
 #[instrument(level = "trace", ret)]
 pub fn extract_user_name_from_subject_name(certificate: &Certificate) -> Result<String> {
     let subject = &certificate.tbs_certificate.subject.0 .0;
@@ -361,6 +373,7 @@ pub fn extract_user_name_from_subject_name(certificate: &Certificate) -> Result<
     let user_name = subject_parts
         .iter()
         .filter(|subject_part| subject_part.0 == oids::at_common_name())
+        // Skip "CN = Users" part
         .skip(1)
         .map(|subject_part| subject_part.1.as_str())
         .next()
@@ -374,8 +387,11 @@ pub fn extract_user_name_from_subject_name(certificate: &Certificate) -> Result<
     Ok(format!("{}@{}", user_name, domain))
 }
 
+// This function extracts the user name from the smart card certificate.
 pub fn extract_user_name_from_certificate(certificate: &Certificate) -> Result<String> {
-    match extract_user_name_from_alt_name(certificate) {
+    // Firstly, it looks at the Subject Alternative Name
+    // because this action doesn't require any data concatenation unlike extracting from the Subject Name.
+    match extract_upn_from_alt_name(certificate) {
         Ok(user_name) => Ok(user_name),
         Err(_) => extract_user_name_from_subject_name(certificate),
     }
@@ -383,36 +399,17 @@ pub fn extract_user_name_from_certificate(certificate: &Certificate) -> Result<S
 
 #[cfg(test)]
 mod tests {
-    use super::{extract_raw_certificate_by_thumbprint, extract_user_name_from_alt_name};
-    use crate::cert_utils::{
-        extract_certificate_by_thumbprint, extract_user_name_from_subject_name, finalize_smart_card_info,
-    };
+    use picky::x509::Cert;
+    use picky_asn1_x509::Certificate;
 
-    #[test]
-    fn smart_card_info() {
-        let serial_number = [
-            126, 0, 0, 0, 15, 203, 194, 190, 102, 29, 163, 34, 144, 0, 0, 0, 0, 0, 15,
-        ];
-        println!("{:?}", unsafe { finalize_smart_card_info(&serial_number).unwrap() });
-    }
-
-    #[test]
-    fn cert() {
-        println!("cert here: {:?}", unsafe {
-            extract_certificate_by_thumbprint(&[60, 51, 235, 194, 72, 148, 15, 37, 176, 168, 245, 241, 146, 185, 12, 11, 235, 139, 141, 82]).unwrap()
-        });
-    }
+    use super::extract_upn_from_alt_name;
 
     #[test]
     fn username_extraction() {
-        let cert = unsafe {
-            extract_certificate_by_thumbprint(&[
-                244, 5, 6, 138, 23, 82, 125, 87, 234, 251, 176, 71, 81, 51, 245, 207, 224, 92, 147, 141,
-            ])
+        let certificate: Certificate = Cert::from_pem_str(include_str!("../test_assets/pw11.cer"))
             .unwrap()
-        };
-        // dbg!(cert.1.clone());
-        println!("username: {}", extract_user_name_from_alt_name(&cert.1).unwrap());
-        println!("username: {}", extract_user_name_from_subject_name(&cert.1).unwrap());
+            .into();
+
+        assert_eq!("pw11@example.com", extract_upn_from_alt_name(&certificate).unwrap());
     }
 }
