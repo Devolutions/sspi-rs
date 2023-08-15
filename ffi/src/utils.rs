9bd7bd3613aa252259eb192995e0c3d380128100
Pavlo Myroniuk <pavlo.myroniuk@apriorit.com>
2023-08-15 19:24:08 +0300

    feat(ffi): utils: remove uneeded function str_to_utf16_bytes;

--- ffi/src/utils.rs
+++ ffi/src/utils.rs
@@ -50,7 +50,3 @@ pub unsafe fn transform_credentials_handle<'a>(
         ))
     }
 }
-
-pub fn str_to_utf16_bytes(s: impl AsRef<str>) -> Vec<u8> {
-    s.as_ref().encode_utf16().flat_map(|v| v.to_le_bytes()).collect()
-}
