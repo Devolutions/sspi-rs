7b52defc23ad5d1f1dc159209bf0786abbe1da34
Pavlo Myroniuk <pavlo.myroniuk@apriorit.com>
2023-09-18 16:23:21 +0300

    refactor(winscard): added more documentation; removed some of the unnamed constants

--- winscard-rs/src/piv_cert.rs
+++ winscard-rs/src/piv_cert.rs
@@ -11,11 +11,14 @@ pub(crate) fn build_auth_cert(auth_cert: Vec<u8>) -> Result<Vec<u8>> {
     // SIMPLE-TLV encoding
     // We do use Tlv::new() here to avoid calculating SIMPLE-TLV length ourselves (as a certificate is most certainly > 254 bytes in length)
     let certificate = Tlv::new(Tag::try_from(tlv_tags::CERTIFICATE)?, auth_cert)?.to_vec();
+    let cert_info_length = 0x01;
     // 0x00 indicates that the certificate is uncompressed
     // NIST.SP.800-73-4, Part 1, Appendix A, table 39
-    let cert_info = &[tlv_tags::CERT_INFO, 0x01, 0x00];
+    let cert_info_value = 0x00;
+    let cert_info = &[tlv_tags::CERT_INFO, cert_info_length, cert_info_value];
     // NIST.SP.800-73-4, Part 1, Appendix A, table 10
-    let edc = &[tlv_tags::ERROR_DETECTION_CODE, 0x00];
+    let edc_length = 0x00;
+    let edc = &[tlv_tags::ERROR_DETECTION_CODE, edc_length];
     let data_value_len = certificate.len() + cert_info.len() + edc.len();
     // BER-TLV encoding of the data_value_len
     let encoded_data_value_len = ber_tlv_length_encoding(data_value_len);
