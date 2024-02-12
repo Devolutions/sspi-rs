7ff3d49b7a4c2c7e3abbad0e0a49e24bee47cec5
Pavlo Myroniuk <pavlo.myroniuk@apriorit.com>
2024-02-12 22:57:05 +0200

    fix: username handling in FFI during smart card logon (#209)

--- ffi/src/sspi/utils.rs
+++ ffi/src/sspi/utils.rs
@@ -20,7 +20,7 @@ pub unsafe fn transform_credentials_handle<'a>(
 
 // when encoding an UTF-16 character using two code units, the 16-bit values are chosen from the UTF-16 surrogate range 0xD800–0xDFFF,
 // and thus only \0 is encoded by two consecutive null bytes
-#[cfg(feature = "tsssp")]
+#[cfg(any(feature = "tsssp", feature = "scard"))]
 pub fn raw_wide_str_trim_nulls(raw_str: &mut Vec<u8>) {
     let mut len = raw_str.len();
     while len > 2 && raw_str[len - 2..] == [0, 0] {
