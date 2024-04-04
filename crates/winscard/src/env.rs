f9f4b7f1c554c2b321027793612f067b06bb1fb0
Pavlo Myroniuk <pavlo.myroniuk@apriorit.com>
2024-04-04 21:25:30 +0300

    feat(winscard): add `WINSCARD_` prefix for env variables;

--- crates/winscard/src/env.rs
+++ crates/winscard/src/env.rs
@@ -1,18 +1,18 @@
 /// Emulated smart card PIN code.
-pub const WINSCARD_PIN_ENV: &str = "SMARTCARD_PIN";
+pub const WINSCARD_PIN_ENV: &str = "WINSCARD_SMARTCARD_PIN";
 /// Path to the user certificate to be used in emulated smart card.
-pub const WINSCARD_CERT_PATH_ENV: &str = "CERTIFICATE_FILE_PATH";
+pub const WINSCARD_CERT_PATH_ENV: &str = "WINSCARD_CERTIFICATE_FILE_PATH";
 /// Smart card certificate data.
 ///
 /// *Note.* The variable value should be one-line base64 string containing ASN1 DER certificate data.
-pub const WINSCARD_CERT_DATA_ENV: &str = "CERTIFICATE_FILE_DATA";
+pub const WINSCARD_CERT_DATA_ENV: &str = "WINSCARD_CERTIFICATE_FILE_DATA";
 /// Path to the certificate private key.
-pub const WINSCARD_PK_PATH_ENV: &str = "PRIVATE_KEY_FILE_PATH";
+pub const WINSCARD_PK_PATH_ENV: &str = "WINSCARD_PRIVATE_KEY_FILE_PATH";
 /// Smart card private key data.
 ///
 /// *Note.* The variable value should be one-line base64 string containing ASN1 DER private key.
-pub const WINSCARD_PK_DATA_ENV: &str = "PRIVATE_KEY_FILE_DATA";
+pub const WINSCARD_PK_DATA_ENV: &str = "WINSCARD_PRIVATE_KEY_FILE_DATA";
 /// Emulated smart card container name.
-pub const WINSCARD_CONTAINER_NAME_ENV: &str = "SMARTCARD_CONTAINER_NAME";
+pub const WINSCARD_CONTAINER_NAME_ENV: &str = "WINSCARD_SMARTCARD_CONTAINER_NAME";
 /// Emulated smart card reader name.
-pub const WINSCARD_READER_NAME_ENV: &str = "SMARTCARD_READER_NAME";
+pub const WINSCARD_READER_NAME_ENV: &str = "WINSCARD_SMARTCARD_READER_NAME";
