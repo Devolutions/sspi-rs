42ba30d26f0150b5dcbb0a92737214d2fb15f7b5
Pavlo Myroniuk <pavlo.myroniuk@apriorit.com>
2024-04-19 18:26:57 +0300

    feat(ffi): winscard: implement `begin_transaction`, `end_transaction`, `reconnect`, `get_attribute`, and `set_attribute` methods for system scard cards on Windows;

--- ffi/src/winscard/system_scard/card.rs
+++ ffi/src/winscard/system_scard/card.rs
@@ -174,20 +174,26 @@ impl WinScard for SystemScard {
 
     fn begin_transaction(&mut self) -> WinScardResult<()> {
         #[cfg(not(target_os = "windows"))]
-        unsafe {
-            try_execute!(pcsc_lite_rs::SCardBeginTransaction(self.h_card))?;
+        {
+            try_execute!(unsafe { pcsc_lite_rs::SCardBeginTransaction(self.h_card) })
         }
-        // TODO(@TheBestTvarynka): implement for Windows too.
-        Ok(())
+        #[cfg(target_os = "windows")]
+        {
+            try_execute!(unsafe { windows_sys::Win32::Security::Credentials::SCardBeginTransaction(self.h_card) })
+        }
     }
 
     fn end_transaction(&mut self, disposition: ReaderAction) -> WinScardResult<()> {
         #[cfg(not(target_os = "windows"))]
-        unsafe {
-            try_execute!(pcsc_lite_rs::SCardEndTransaction(self.h_card, disposition.into()))?;
+        {
+            try_execute!(unsafe { pcsc_lite_rs::SCardEndTransaction(self.h_card, disposition.into()) })
         }
-        // TODO(@TheBestTvarynka): implement for Windows too.
-        Ok(())
+        #[cfg(target_os = "windows")]
+        {
+            try_execute!(unsafe {
+                windows_sys::Win32::Security::Credentials::SCardEndTransaction(self.h_card, disposition.into())
+            })
+        }
     }
 
     fn reconnect(
@@ -196,11 +202,11 @@ impl WinScard for SystemScard {
         preferred_protocol: Option<Protocol>,
         initialization: ReaderAction,
     ) -> WinScardResult<Protocol> {
+        let dw_preferred_protocols = preferred_protocol.unwrap_or_default().bits();
+        let mut active_protocol = 0;
+
         #[cfg(not(target_os = "windows"))]
         {
-            let dw_preferred_protocols = preferred_protocol.unwrap_or_default().bits();
-            let mut active_protocol = 0;
-
             try_execute!(unsafe {
                 pcsc_lite_rs::SCardReconnect(
                     self.h_card,
@@ -210,43 +216,75 @@ impl WinScard for SystemScard {
                     &mut active_protocol,
                 )
             })?;
-
-            Ok(Protocol::from_bits(active_protocol).unwrap_or_default())
         }
         #[cfg(target_os = "windows")]
         {
-            // TODO(@TheBestTvarynka): implement for Windows too.
-            todo!()
+            try_execute!(unsafe {
+                windows_sys::Win32::Security::Credentials::SCardReconnect(
+                    self.h_card,
+                    share_mode.into(),
+                    dw_preferred_protocols,
+                    initialization.into(),
+                    &mut active_protocol,
+                )
+            })?;
         }
+
+        Ok(Protocol::from_bits(active_protocol).unwrap_or_default())
     }
 
     fn get_attribute(&self, attribute_id: AttributeId) -> WinScardResult<Cow<[u8]>> {
         let attr_id = attribute_id
             .to_u32()
             .ok_or_else(|| Error::new(ErrorKind::InternalError, "Cannot convert AttributeId -> u32"))?;
+        let mut data_len = 0;
 
         #[cfg(not(target_os = "windows"))]
         {
-            let mut data_len = 0;
-
-            // [SCardGetAttrib](https://pcsclite.apdu.fr/api/group__API.html#gaacfec51917255b7a25b94c5104961602)
+            // https://pcsclite.apdu.fr/api/group__API.html#gaacfec51917255b7a25b94c5104961602
             //
             // If this value is NULL, SCardGetAttrib() ignores the buffer length supplied in pcbAttrLen, writes the length of the buffer
             // that would have been returned if this parameter had not been NULL to pcbAttrLen, and returns a success code.
             try_execute!(unsafe { pcsc_lite_rs::SCardGetAttrib(self.h_card, attr_id, null_mut(), &mut data_len) })?;
-
-            let mut data = vec![0; data_len.try_into()?];
-            try_execute!(unsafe {
-                pcsc_lite_rs::SCardGetAttrib(self.h_card, attr_id, data.as_mut_ptr(), &mut data_len)
-            })?;
-
-            Ok(Cow::Owned(data))
         }
         #[cfg(target_os = "windows")]
         {
-            // TODO(@TheBestTvarynka): implement for Windows too.
-            todo!()
+            // https://learn.microsoft.com/en-us/windows/win32/api/winscard/nf-winscard-scardgetattrib
+            //
+            // If this value is NULL, SCardGetAttrib ignores the buffer length supplied in pcbAttrLen,
+            // writes the length of the buffer that would have been returned if this parameter
+            // had not been NULL to pcbAttrLen, and returns a success code.
+            try_execute!(unsafe {
+                windows_sys::Win32::Security::Credentials::SCardGetAttrib(
+                    self.h_card,
+                    attr_id,
+                    null_mut(),
+                    &mut data_len,
+                )
+            })?;
         }
+
+        let mut data = vec![0; data_len.try_into()?];
+
+        #[cfg(not(target_os = "windows"))]
+        {
+            try_execute!(unsafe {
+                pcsc_lite_rs::SCardGetAttrib(self.h_card, attr_id, data.as_mut_ptr(), &mut data_len)
+            })?;
+        }
+        #[cfg(target_os = "windows")]
+        {
+            try_execute!(unsafe {
+                windows_sys::Win32::Security::Credentials::SCardGetAttrib(
+                    self.h_card,
+                    attr_id,
+                    data.as_mut_ptr(),
+                    &mut data_len,
+                )
+            })?;
+        }
+
+        Ok(Cow::Owned(data))
     }
 
     fn set_attribute(&mut self, attribute_id: AttributeId, attribute_data: &[u8]) -> WinScardResult<()> {
@@ -254,15 +292,22 @@ impl WinScard for SystemScard {
             .to_u32()
             .ok_or_else(|| Error::new(ErrorKind::InternalError, "Cannot convert AttributeId -> u32"))?;
 
+        let len = attribute_data.len().try_into()?;
+
         #[cfg(not(target_os = "windows"))]
         {
-            let len = attribute_data.len().try_into().unwrap();
-            try_execute!(unsafe { pcsc_lite_rs::SCardSetAttrib(self.h_card, attr_id, attribute_data.as_ptr(), len) })?;
+            try_execute!(unsafe { pcsc_lite_rs::SCardSetAttrib(self.h_card, attr_id, attribute_data.as_ptr(), len) })
         }
         #[cfg(target_os = "windows")]
         {
-            // TODO(@TheBestTvarynka): implement for Windows too.
+            try_execute!(unsafe {
+                windows_sys::Win32::Security::Credentials::SCardSetAttrib(
+                    self.h_card,
+                    attr_id,
+                    attribute_data.as_ptr(),
+                    len,
+                )
+            })
         }
-        Ok(())
     }
 }
