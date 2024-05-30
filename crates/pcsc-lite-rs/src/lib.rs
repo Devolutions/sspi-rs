fcc466a22a777889b9741d178b1438e3701f6035
Pavlo Myroniuk <pavlo.myroniuk@apriorit.com>
2024-05-30 11:31:03 +0300

    feat(pcsc-lite-rs): improve bindings: context and hard handles;

--- crates/pcsc-lite-rs/src/lib.rs
+++ crates/pcsc-lite-rs/src/lib.rs
@@ -5,9 +5,23 @@
 compile_error!("The pcsc-lite-rs crate should be used only on Linux/MacOS. Use windows/windows-sys crated to interact with Windows API.");
 
 use ffi_types::winscard::{
-    LpScardContext, LpScardHandle, ScardContext, ScardHandle, ScardIoRequest, ScardReaderStateA, ScardStatus,
+    ScardIoRequest, ScardReaderStateA, ScardStatus,
 };
 use ffi_types::{Dword, LpByte, LpCByte, LpCStr, LpCVoid, LpDword, LpStr, LpVoid};
+
+/// `hContext` returned by `SCardEstablishContext()`.
+///
+/// https://pcsclite.apdu.fr/api/pcsclite_8h.html#a22530ffaff18b5d3e32260a5f1ce4abd
+pub type ScardContext = i32;
+/// Pointer to the [ScardContext].
+pub type LpScardContext = *mut ScardContext;
+
+/// `hCard` returned by `SCardConnect()`.
+///
+/// https://pcsclite.apdu.fr/api/pcsclite_8h.html#af328aca3e11de737ecd771bcf1f75fb5
+pub type ScardHandle = i32;
+/// Pointer to the [ScardHandle].
+pub type LpScardHandle = *mut ScardHandle;
 
 extern "system" {
     /// Creates an Application Context to the PC/SC Resource Manager.
