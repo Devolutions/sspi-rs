06223d6031d4fb33f80900f69d14130682e765ac
Pavlo Myroniuk <pavlo.myroniuk@apriorit.com>
2024-10-03 11:11:09 +0300

    feat(ffi): winscard: system scard: add comment about `unwrap` call;

--- ffi/src/winscard/system_scard/macros.rs
+++ ffi/src/winscard/system_scard/macros.rs
@@ -6,6 +6,9 @@ macro_rules! try_execute {
         let error_kind = ErrorKind::from_u32(
             // In pcsc-lite API, the status code has 8-byte width. But the Windows WinSCard uses 4-byte width status code.
             #[allow(clippy::useless_conversion)]
+            // `pcsc-lite` status codes has 8-byte width, but the values always fit 4-byte number:
+            // https://pcsclite.apdu.fr/api/group__ErrorCodes.html#details
+            // This `.unwrap` will never panic.
             $x.try_into().unwrap()
         ).unwrap_or(ErrorKind::InternalError);
         if error_kind == ErrorKind::Success {
