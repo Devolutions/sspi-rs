09b079097727b82f81ef3b65c5043e1185378cd5
Pavlo Myroniuk <pavlo.myroniuk@apriorit.com>
2024-11-28 19:12:34 +0200

    feat(dpapi): rpc: implement `SyntaxId`. add basic traits for decoding/encoding. implement `BindTimeFeatureNegotiationBitmask`;

---
+++ crates/dpapi/src/error.rs
@@ -0,0 +1,50 @@
+use std::io::Error as IoError;
+
+use num_derive::{FromPrimitive, ToPrimitive};
+use uuid::Error as UuidError;
+
+#[derive(Debug, Copy, Clone, Eq, PartialEq, FromPrimitive, ToPrimitive)]
+#[repr(u32)]
+pub enum ErrorKind {
+    Success = 0,
+    NteBadFlags = 0x80090009,
+    NteInvalidParameter = 0x80090027,
+
+    IoError = 1,
+    UuidError = 2,
+}
+
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub struct Error {
+    pub kind: ErrorKind,
+    pub description: String,
+}
+
+impl Error {
+    pub fn new(kind: ErrorKind, description: impl Into<String>) -> Self {
+        Self {
+            kind,
+            description: description.into(),
+        }
+    }
+}
+
+pub type DpapiResult<T> = Result<T, Error>;
+
+impl From<IoError> for Error {
+    fn from(err: IoError) -> Self {
+        Self {
+            kind: ErrorKind::IoError,
+            description: err.to_string(),
+        }
+    }
+}
+
+impl From<UuidError> for Error {
+    fn from(err: UuidError) -> Self {
+        Self {
+            kind: ErrorKind::UuidError,
+            description: err.to_string(),
+        }
+    }
+}
---
+++ crates/dpapi/src/rpc/mod.rs
@@ -0,0 +1,20 @@
+mod bind;
+
+use std::io::{Read, Write};
+
+use crate::DpapiResult;
+
+trait Encode {
+    fn encode(&self, writer: impl Write) -> DpapiResult<()>;
+    fn encode_to_vec(&self) -> DpapiResult<Vec<u8>> {
+        let mut buf = Vec::new();
+
+        self.encode(&mut buf)?;
+
+        Ok(buf)
+    }
+}
+
+trait Decode: Sized {
+    fn decode(reader: impl Read) -> DpapiResult<Self>;
+}
