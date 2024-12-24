a85d2cf0fd9ca8537fddcec46498a9fe9f7c34fd
Pavlo Myroniuk <pavlo.myroniuk@apriorit.com>
2024-12-24 14:24:30 +0200

    refactor(dpapi): small refactoring;

--- crates/dpapi/src/error.rs
+++ crates/dpapi/src/error.rs
@@ -8,41 +8,44 @@ pub enum Error {
     #[error("UUID error: {0:?}")]
     Uuid(#[from] uuid::Error),
 
-    #[error("Integer conversion error: {0:?}")]
+    #[error("integer conversion error: {0:?}")]
     IntegerConversion(#[from] std::num::TryFromIntError),
 
-    #[error("Provided buf contains invalid UTF-8 data: {0:?}")]
+    #[error("provided buf contains invalid UTF-8 data: {0:?}")]
     Utf8(#[from] std::string::FromUtf8Error),
 
-    #[error("Invalid context result code value: {0}")]
+    #[error("invalid context result code value: {0}")]
     InvalidContextResultCode(u16),
 
-    #[error("Invalid integer representation value: {0}")]
+    #[error("invalid integer representation value: {0}")]
     InvalidIntegerRepresentation(u8),
 
-    #[error("Invalid character representation value: {0}")]
+    #[error("invalid character representation value: {0}")]
     InvalidCharacterRepresentation(u8),
 
-    #[error("Invalid floating point representation value: {0}")]
+    #[error("invalid floating point representation value: {0}")]
     InvalidFloatingPointRepresentation(u8),
 
-    #[error("Invalid packet type value: {0}")]
+    #[error("invalid packet type value: {0}")]
     InvalidPacketType(u8),
 
-    #[error("Invalid packet flags value: {0}")]
+    #[error("invalid packet flags value: {0}")]
     InvalidPacketFlags(u8),
 
-    #[error("Invalid security provider value: {0}")]
+    #[error("invalid security provider value: {0}")]
     InvalidSecurityProvider(u8),
 
-    #[error("Invalid authentication level value: {0}")]
+    #[error("invalid authentication level value: {0}")]
     InvalidAuthenticationLevel(u8),
 
-    #[error("Invalid fault flags value: {0}")]
+    #[error("invalid fault flags value: {0}")]
     InvalidFaultFlags(u8),
 
     #[error("{0:?} PDU is not supported")]
     PduNotSupported(crate::rpc::pdu::PacketType),
+
+    #[error("invalid fragment (PDU) length: {0}")]
+    InvalidFragLength(u16),
 }
 
 pub type DpapiResult<T> = Result<T, Error>;
