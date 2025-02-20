807b71e70a6cc83cb99cec29a85650e60dd0e002
Pavlo Myroniuk <pavlo.myroniuk@apriorit.com>
2025-02-20 21:24:54 +0200

    feat(dpapi-cli): implement simple DPAPI cli client;

---
+++ crates/dpapi-cli-client/src/cli.rs
@@ -0,0 +1,64 @@
+use std::path::PathBuf;
+
+use clap::{Parser, Subcommand};
+
+#[derive(Debug, Subcommand)]
+pub enum Command {
+    /// Encrypt secret.
+    ///
+    /// This command simulated the `NCryptProtectSecret` function. The encrypted secret (DPAPI blob) will be printed to stdout.
+    Encrypt {
+        /// User's SID.
+        #[arg(long, short = 'i')]
+        sid: String,
+
+        /// Secret to encrypt.
+        ///
+        /// This parameter is optional. If not provided, the app will try to read the secret from stdin.
+        #[arg(long)]
+        secret: Option<String>,
+    },
+
+    /// Decrypt DPAPI blob.
+    ///
+    /// This command simulated the `NCryptUnprotectSecret` function. The decrypted secret will be printed to stdout.
+    Decrypt {
+        /// Path to file that contains DPAPI blob.
+        ///
+        /// This parameter is optional. If not provided, the app will try to read the DPAPI blob from stdin.
+        #[arg(long, short = 'f')]
+        file: Option<PathBuf>,
+    },
+}
+
+/// DPAPI cli client.
+///
+/// This app is used to encrypt/decrypt secrets using the DPAPI.
+#[derive(Debug, Parser)]
+#[command(version, about)]
+pub struct Cli {
+    #[command(subcommand)]
+    pub command: Command,
+
+    /// Target server hostname.
+    ///
+    /// For example, `win-956cqossjtf.tbt.com`.
+    #[arg(long, short = 's')]
+    pub server: String,
+
+    /// Username the username to decrypt/encrypt the DPAPI blob.
+    ///
+    /// The username can be specified in FQDN (DOMAIN\username) or UPN (username@domain) format.
+    #[arg(long, short = 'u')]
+    pub username: String,
+
+    /// User's password.
+    #[arg(long, short = 'p')]
+    pub password: String,
+
+    /// Client computer name.
+    ///
+    /// This parameter is optional. If not provided, the current computer name will be used.
+    #[arg(long)]
+    pub computer_name: Option<String>,
+}
---
+++ crates/dpapi-cli-client/src/main.rs
@@ -0,0 +1,48 @@
+mod cli;
+
+use std::fs;
+use std::io::{stdin, stdout, Read, Result, Write};
+
+use clap::Parser;
+
+use self::cli::{Cli, Command};
+
+fn main() -> Result<()> {
+    let Cli {
+        command,
+        server,
+        username,
+        password,
+        computer_name,
+    } = Cli::parse();
+
+    match command {
+        Command::Encrypt { sid, secret } => {
+            let secret = if let Some(secret) = secret {
+                secret.into_bytes()
+            } else {
+                stdin().bytes().collect::<Result<Vec<_>>>()?
+            };
+
+            let blob =
+                dpapi::n_crypt_protect_secret(&secret, sid, None, &server, &username, password.into(), computer_name)
+                    .unwrap();
+
+            stdout().write_all(&blob)?;
+        }
+        Command::Decrypt { file } => {
+            let blob = if let Some(file) = file {
+                fs::read(file)?
+            } else {
+                stdin().bytes().collect::<Result<Vec<_>>>()?
+            };
+
+            let secret =
+                dpapi::n_crypt_unprotect_secret(&blob, &server, &username, password.into(), computer_name).unwrap();
+
+            stdout().write_all(&secret)?;
+        }
+    }
+
+    Ok(())
+}
