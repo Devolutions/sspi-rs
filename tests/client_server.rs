58539581a7463b4d56770e059b8fb77b32f2238b
Pavlo Myroniuk <pavlo.myroniuk@apriorit.com>
2025-03-26 12:57:03 +0200

    feat(tests): finish NTLM client-server smoke test;

--- tests/client_server.rs
+++ tests/client_server.rs
@@ -1,10 +1,29 @@
-use sspi::{Ntlm, AuthIdentity, AcquireCredentialsHandleResult, CredentialUse, Username, Secret, Sspi, SspiImpl, ClientRequestFlags, SecurityBuffer, BufferType, DataRepresentation, SecurityStatus, Credentials, InitializeSecurityContextResult};
-use sspi::credssp::SspiContext;
 use sspi::builders::{AcquireCredentialsHandle, WithoutCredentialUse};
+use sspi::credssp::SspiContext;
 use sspi::ntlm::NtlmConfig;
+use sspi::{
+    AcquireCredentialsHandleResult, AuthIdentity, BufferType, ClientRequestFlags, CredentialUse, Credentials,
+    DataRepresentation, EncryptionFlags, InitializeSecurityContextResult, Ntlm, Secret, SecurityBuffer,
+    SecurityBufferRef, SecurityStatus, ServerRequestFlags, Sspi, Username,
+};
 
-fn encrypt() {
-    //
+fn test_ntlm_encryption(client: &mut SspiContext, server: &mut SspiContext) {
+    let plain_message = b"Devolutions/sspi-rs";
+
+    let mut token = [0; 1024];
+    let mut data = plain_message.to_vec();
+
+    let mut message = vec![
+        SecurityBufferRef::token_buf(token.as_mut_slice()),
+        SecurityBufferRef::data_buf(data.as_mut_slice()),
+    ];
+
+    client
+        .encrypt_message(EncryptionFlags::empty(), &mut message, 0)
+        .unwrap();
+    server.decrypt_message(&mut message, 0).unwrap();
+
+    assert_eq!(plain_message, message[1].data());
 }
 
 fn run_ntlm(config: NtlmConfig) {
@@ -16,16 +35,22 @@ fn run_ntlm(config: NtlmConfig) {
 
     let mut client = SspiContext::Ntlm(Ntlm::with_config(config.clone()));
     let mut server = SspiContext::Ntlm(Ntlm::with_config(config));
-    
+
     let builder = AcquireCredentialsHandle::<'_, _, _, WithoutCredentialUse>::new();
-    let AcquireCredentialsHandleResult { credentials_handle: mut client_credentials_handle, .. } = builder
+    let AcquireCredentialsHandleResult {
+        credentials_handle: mut client_credentials_handle,
+        ..
+    } = builder
         .with_auth_data(&credentials)
         .with_credential_use(CredentialUse::Outbound)
         .execute(&mut client)
         .unwrap();
 
     let builder = AcquireCredentialsHandle::<'_, _, _, WithoutCredentialUse>::new();
-    let AcquireCredentialsHandleResult { credentials_handle: mut server_credentials_handle, .. } = builder
+    let AcquireCredentialsHandleResult {
+        credentials_handle: mut server_credentials_handle,
+        ..
+    } = builder
         .with_auth_data(&credentials)
         .with_credential_use(CredentialUse::Outbound)
         .execute(&mut server)
@@ -34,48 +59,44 @@ fn run_ntlm(config: NtlmConfig) {
     let mut input_token = [SecurityBuffer::new(Vec::new(), BufferType::Token)];
     let mut output_token = [SecurityBuffer::new(Vec::new(), BufferType::Token)];
 
-    for i in 0..5 {
-        println!("i: {i}");
+    for _ in 0..3 {
         let mut builder = client
             .initialize_security_context()
             .with_credentials_handle(&mut client_credentials_handle)
             .with_context_requirements(
                 ClientRequestFlags::MUTUAL_AUTH
-                | ClientRequestFlags::USE_SESSION_KEY
-                | ClientRequestFlags::INTEGRITY
-                | ClientRequestFlags::CONFIDENTIALITY,
+                    | ClientRequestFlags::USE_SESSION_KEY
+                    | ClientRequestFlags::INTEGRITY
+                    | ClientRequestFlags::CONFIDENTIALITY,
             )
             .with_target_data_representation(DataRepresentation::Native)
             .with_target_name(target_name)
             .with_input(&mut input_token)
             .with_output(&mut output_token);
-        let InitializeSecurityContextResult { status, .. } = client.initialize_security_context_sync(&mut builder).unwrap();
-        
-        if status == SecurityStatus::Ok {
-            encrypt();
-        }
+        let InitializeSecurityContextResult { status, .. } =
+            client.initialize_security_context_sync(&mut builder).unwrap();
 
         input_token[0].buffer.clear();
 
-        let mut builder = server
-            .initialize_security_context()
+        server
+            .accept_security_context()
             .with_credentials_handle(&mut server_credentials_handle)
-            .with_context_requirements(
-                ClientRequestFlags::MUTUAL_AUTH
-                | ClientRequestFlags::USE_SESSION_KEY
-                | ClientRequestFlags::INTEGRITY
-                | ClientRequestFlags::CONFIDENTIALITY,
-            )
+            .with_context_requirements(ServerRequestFlags::empty())
             .with_target_data_representation(DataRepresentation::Native)
-            .with_target_name(target_name)
             .with_input(&mut output_token)
-            .with_output(&mut input_token);
-        server.initialize_security_context_sync(&mut builder).unwrap();
+            .with_output(&mut input_token)
+            .execute(&mut server)
+            .unwrap();
 
         output_token[0].buffer.clear();
+
+        if status == SecurityStatus::Ok {
+            test_ntlm_encryption(&mut client, &mut server);
+            return;
+        }
     }
 
-    panic!("NTLM authentication should not exceed 5 steps")
+    panic!("NTLM authentication should not exceed 3 steps")
 }
 
 #[test]
