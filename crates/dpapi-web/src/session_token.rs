c554f699568cd9ce460230be013aefda3fe06767
Pavlo Myroniuk <pavlo.myroniuk@apriorit.com>
2025-04-18 00:23:02 +0300

    feat(dpapi-web): make JS get session token fn async;

--- crates/dpapi-web/src/session_token.rs
+++ crates/dpapi-web/src/session_token.rs
@@ -4,6 +4,7 @@ use dpapi_transport::GetSessionTokenFn;
 use url::Url;
 use uuid::Uuid;
 use wasm_bindgen::prelude::*;
+use wasm_bindgen_futures::JsFuture;
 
 pub fn session_token_fn(get_session_token: js_sys::Function) -> Box<GetSessionTokenFn> {
     Box::new(move |session_id: Uuid, destination: Url| {
@@ -12,8 +13,14 @@ pub fn session_token_fn(get_session_token: js_sys::Function) -> Box<GetSessionTo
             let session_id = JsValue::from_str(&session_id.to_string());
             let destination = JsValue::from_str(destination.as_str());
 
-            let session_token = get_session_token
+            let promise = get_session_token
                 .call2(&JsValue::NULL, &session_id, &destination)
+                .map_err(|err| Error::other(format!("failed to obtain the session token: {:?}", err)))?
+                .dyn_into::<js_sys::Promise>()
+                .map_err(|err| Error::other(format!("failed to obtain the session token: {:?}", err)))?;
+
+            let session_token = JsFuture::from(promise)
+                .await
                 .map_err(|err| Error::other(format!("failed to obtain the session token: {:?}", err)))?;
 
             Ok(session_token
