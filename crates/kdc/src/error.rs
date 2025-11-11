08d5e759ddae501601045dc403f910aecd6c8c60
Pavlo Myroniuk <pavlo.myroniuk@apriorit.com>
2025-11-11 09:35:55 +0200

    feat(kdc): add initial KDC implementation;

---
+++ crates/kdc/src/error.rs
@@ -0,0 +1,223 @@
+use picky_asn1::date::GeneralizedTime;
+use picky_asn1::restricted_string::IA5String;
+use picky_asn1::wrapper::{
+    Asn1SequenceOf, ExplicitContextTag0, ExplicitContextTag1, ExplicitContextTag2, ExplicitContextTag4,
+    ExplicitContextTag5, ExplicitContextTag6, ExplicitContextTag7, ExplicitContextTag9, ExplicitContextTag10,
+    ExplicitContextTag11, ExplicitContextTag12, IntegerAsn1, OctetStringAsn1, Optional,
+};
+use picky_asn1_der::Asn1DerError;
+use picky_krb::constants::error_codes::{
+    KDC_ERR_C_PRINCIPAL_UNKNOWN, KDC_ERR_CANNOT_POSTDATE, KDC_ERR_ETYPE_NOSUPP, KDC_ERR_NEVER_VALID,
+    KDC_ERR_PREAUTH_FAILED, KDC_ERR_PREAUTH_REQUIRED, KDC_ERR_S_PRINCIPAL_UNKNOWN, KDC_ERR_WRONG_REALM,
+    KRB_AP_ERR_BADVERSION, KRB_AP_ERR_MODIFIED, KRB_AP_ERR_MSG_TYPE, KRB_AP_ERR_SKEW, KRB_AP_ERR_TKT_EXPIRED,
+    KRB_ERR_GENERIC,
+};
+use picky_krb::constants::types::{KRB_ERROR_MSG_TYPE, NT_SRV_INST, PA_ENC_TIMESTAMP, PA_ETYPE_INFO2_TYPE};
+use picky_krb::crypto::CipherSuite;
+use picky_krb::data_types::{
+    EtypeInfo2Entry, KerberosStringAsn1, KerberosTime, Microseconds, PaData, PrincipalName, Realm,
+};
+use picky_krb::messages::{KdcReqBody, KrbError, KrbErrorInner};
+use sspi::KERBEROS_VERSION;
+use sspi::kerberos::TGT_SERVICE_NAME;
+use thiserror::Error;
+use time::OffsetDateTime;
+
+use crate::config::KerberosServer;
+use crate::find_user_credentials;
+
+#[derive(Error, Debug)]
+pub(super) enum KdcError {
+    #[error("KRB_AP_ERR_BADVERSION: got invalid Kerberos version ({version:?}): expected [{expected}]")]
+    BadKrbVersion { version: Vec<u8>, expected: u8 },
+
+    #[error("KRB_AP_ERR_MSG_TYPE: got invalid Kerberos message type ({msg_type:?}): expected [{expected}]")]
+    BadMsgType { msg_type: Vec<u8>, expected: u8 },
+
+    #[error("KDC_ERR_WRONG_REALM: wrong realm: {0}")]
+    WrongRealm(String),
+
+    #[error("ASN1 DER encoding failed: {0:?}")]
+    Asn1Encode(#[from] Asn1DerError),
+
+    #[error("encryption failed: {0:?}")]
+    EncryptionFailed(#[from] picky_krb::crypto::KerberosCryptoError),
+
+    #[error("KDC_ERR_C_PRINCIPAL_UNKNOWN: {0}")]
+    ClientPrincipalUnknown(String),
+
+    #[error("invalid cname type: {0:?}")]
+    InvalidCnameType(Vec<u8>),
+
+    #[error("invalid sname type: {0:?}")]
+    InvalidSnameType(Vec<u8>),
+
+    #[error("invalid sname: {0}")]
+    InvalidSname(String),
+
+    #[error("KDC_ERR_ETYPE_NOSUPP: only AES256_CTS_HMAC_SHA1_96 and AES128_CTS_HMAC_SHA1_96 etypes are supported")]
+    NoSuitableEtype,
+
+    #[error("KDC_ERR_PREAUTH_FAILED: {0}")]
+    PreAuthFailed(&'static str),
+
+    #[error("KDC_ERR_PREAUTH_REQUIRED: {0}")]
+    PreAuthRequired(&'static str),
+
+    #[error("KRB_ERR_GENERIC: internal error: {0}")]
+    InternalError(&'static str),
+
+    #[error("KRB_AP_ERR_SKEW: {0}")]
+    ClockSkew(&'static str),
+
+    #[error("KRB_AP_ERR_MODIFIED: {0} decryption failed")]
+    Modified(&'static str),
+
+    #[error("KDC_ERR_CANNOT_POSTDATE: {0}")]
+    CannotPostdate(&'static str),
+
+    #[error("KDC_ERR_NEVER_VALID: {0}")]
+    NeverValid(String),
+
+    #[error("KRB_AP_ERR_TKT_EXPIRED: {0}")]
+    TicketExpired(&'static str),
+}
+
+impl KdcError {
+    pub(super) fn invalid_raw_krb_message_error(kdc_realm: String) -> KrbError {
+        let realm =
+            Realm::from(IA5String::from_string(kdc_realm).expect("configured realm should be valid Kerberos string"));
+
+        let current_date = OffsetDateTime::now_utc();
+        // https://www.rfc-editor.org/rfc/rfc4120#section-5.2.4
+        // Microseconds    ::= INTEGER (0..999999)
+        let microseconds = current_date.microsecond().min(999_999);
+
+        KrbError::from(KrbErrorInner {
+            pvno: ExplicitContextTag0::from(IntegerAsn1(vec![KERBEROS_VERSION])),
+            msg_type: ExplicitContextTag1::from(IntegerAsn1::from(vec![KRB_ERROR_MSG_TYPE])),
+            ctime: Optional::from(None),
+            cusec: Optional::from(None),
+            stime: ExplicitContextTag4::from(KerberosTime::from(GeneralizedTime::from(current_date))),
+            susec: ExplicitContextTag5::from(Microseconds::from(microseconds.to_be_bytes().to_vec())),
+            error_code: ExplicitContextTag6::from(KRB_ERR_GENERIC),
+            crealm: Optional::from(None),
+            cname: Optional::from(None),
+            realm: ExplicitContextTag9::from(realm.clone()),
+            sname: ExplicitContextTag10::from(PrincipalName {
+                name_type: ExplicitContextTag0::from(IntegerAsn1::from(vec![NT_SRV_INST])),
+                name_string: ExplicitContextTag1::from(Asn1SequenceOf::from(vec![
+                    KerberosStringAsn1::from(
+                        IA5String::from_string(TGT_SERVICE_NAME.to_owned())
+                            .expect("TBT_SERVICE_NAME is valid KerberosString"),
+                    ),
+                    realm,
+                ])),
+            }),
+            e_text: Optional::from(Some(ExplicitContextTag11::from(KerberosStringAsn1::from(
+                IA5String::from_string("input message is not valid AS_REQ nor TGS_REQ".to_owned())
+                    .expect("valid Kerberos string"),
+            )))),
+            e_data: Optional::from(None),
+        })
+    }
+
+    pub(super) fn into_krb_error(self, kdc_body: &KdcReqBody, kdc_config: &KerberosServer) -> KrbError {
+        let realm = kdc_body.realm.0.to_string();
+        let cname = kdc_body.cname.0.as_ref().map(|cname| cname.0.clone());
+        let salt = cname.and_then(|cname| {
+            find_user_credentials(&cname, &realm, kdc_config)
+                .map(|user| &user.salt)
+                .ok()
+        });
+        let realm =
+            Realm::from(IA5String::from_string(realm).expect("configured realm should be valid Kerberos string"));
+
+        let sname = kdc_body
+            .sname
+            .0
+            .as_ref()
+            .map(|sname| sname.0.clone())
+            .unwrap_or_else(|| PrincipalName {
+                name_type: ExplicitContextTag0::from(IntegerAsn1::from(vec![NT_SRV_INST])),
+                name_string: ExplicitContextTag1::from(Asn1SequenceOf::from(vec![
+                    KerberosStringAsn1::from(
+                        IA5String::from_string(TGT_SERVICE_NAME.to_owned())
+                            .expect("TGT_SERVICE_NAME is valid KerberosString"),
+                    ),
+                    realm.clone(),
+                ])),
+            });
+
+        let current_date = OffsetDateTime::now_utc();
+        // https://www.rfc-editor.org/rfc/rfc4120#section-5.2.4
+        // Microseconds    ::= INTEGER (0..999999)
+        let microseconds = current_date.microsecond().min(999_999);
+
+        let error_code = match self {
+            KdcError::ClientPrincipalUnknown(_) => KDC_ERR_C_PRINCIPAL_UNKNOWN,
+            KdcError::InvalidCnameType(_) => KDC_ERR_C_PRINCIPAL_UNKNOWN,
+            KdcError::InvalidSnameType(_) => KDC_ERR_S_PRINCIPAL_UNKNOWN,
+            KdcError::InvalidSname(_) => KDC_ERR_S_PRINCIPAL_UNKNOWN,
+            KdcError::NoSuitableEtype => KDC_ERR_ETYPE_NOSUPP,
+            KdcError::PreAuthFailed(_) => KDC_ERR_PREAUTH_FAILED,
+            KdcError::PreAuthRequired(_) => KDC_ERR_PREAUTH_REQUIRED,
+            KdcError::InternalError(_) => KRB_ERR_GENERIC,
+            KdcError::ClockSkew(_) => KRB_AP_ERR_SKEW,
+            KdcError::Modified(_) => KRB_AP_ERR_MODIFIED,
+            KdcError::Asn1Encode(_) => KRB_ERR_GENERIC,
+            KdcError::EncryptionFailed(_) => KRB_ERR_GENERIC,
+            KdcError::BadKrbVersion { .. } => KRB_AP_ERR_BADVERSION,
+            KdcError::BadMsgType { .. } => KRB_AP_ERR_MSG_TYPE,
+            KdcError::WrongRealm(_) => KDC_ERR_WRONG_REALM,
+            KdcError::CannotPostdate(_) => KDC_ERR_CANNOT_POSTDATE,
+            KdcError::NeverValid(_) => KDC_ERR_NEVER_VALID,
+            KdcError::TicketExpired(_) => KRB_AP_ERR_TKT_EXPIRED,
+        };
+
+        let salt = if let Some(salt) = salt {
+            vec![
+                PaData {
+                    padata_type: ExplicitContextTag1::from(IntegerAsn1::from(PA_ETYPE_INFO2_TYPE.to_vec())),
+                    padata_data: ExplicitContextTag2::from(OctetStringAsn1::from(
+                        picky_asn1_der::to_vec(&Asn1SequenceOf::from(vec![EtypeInfo2Entry {
+                            etype: ExplicitContextTag0::from(IntegerAsn1::from(vec![u8::from(
+                                CipherSuite::Aes256CtsHmacSha196,
+                            )])),
+                            salt: Optional::from(Some(ExplicitContextTag1::from(KerberosStringAsn1::from(
+                                IA5String::from_string(salt.to_owned()).expect("salt to be valid KerberosString"),
+                            )))),
+                            s2kparams: Optional::from(None),
+                        }]))
+                        .unwrap_or_else(|_| Vec::new()),
+                    )),
+                },
+                PaData {
+                    padata_type: ExplicitContextTag1::from(IntegerAsn1::from(PA_ENC_TIMESTAMP.to_vec())),
+                    padata_data: ExplicitContextTag2::from(OctetStringAsn1::from(Vec::new())),
+                },
+            ]
+        } else {
+            Vec::new()
+        };
+        let e_data = picky_asn1_der::to_vec(&Asn1SequenceOf::from(salt)).unwrap_or_else(|_| Vec::new());
+
+        KrbError::from(KrbErrorInner {
+            pvno: ExplicitContextTag0::from(IntegerAsn1(vec![KERBEROS_VERSION])),
+            msg_type: ExplicitContextTag1::from(IntegerAsn1::from(vec![KRB_ERROR_MSG_TYPE])),
+            ctime: Optional::from(None),
+            cusec: Optional::from(None),
+            stime: ExplicitContextTag4::from(KerberosTime::from(GeneralizedTime::from(current_date))),
+            susec: ExplicitContextTag5::from(Microseconds::from(microseconds.to_be_bytes().to_vec())),
+            error_code: ExplicitContextTag6::from(error_code),
+            crealm: Optional::from(Some(ExplicitContextTag7::from(realm.clone()))),
+            cname: Optional::from(None),
+            realm: ExplicitContextTag9::from(realm),
+            sname: ExplicitContextTag10::from(sname),
+            e_text: Optional::from(Some(ExplicitContextTag11::from(KerberosStringAsn1::from(
+                IA5String::from_string(self.to_string()).expect("error message to be valid KerberosString"),
+            )))),
+            e_data: Optional::from(Some(ExplicitContextTag12::from(OctetStringAsn1::from(e_data)))),
+        })
+    }
+}
---
+++ crates/kdc/src/lib.rs
@@ -0,0 +1,186 @@
+mod as_exchange;
+mod config;
+mod error;
+mod tgs_exchange;
+mod ticket;
+
+use std::time::Duration;
+
+use picky_asn1::wrapper::{ExplicitContextTag0, GeneralizedTimeAsn1, OctetStringAsn1};
+use picky_asn1_der::Asn1DerError;
+use picky_krb::constants::types::{NT_ENTERPRISE, NT_PRINCIPAL, NT_SRV_INST};
+use picky_krb::data_types::PrincipalName;
+use picky_krb::messages::{AsReq, KdcProxyMessage, TgsReq};
+use time::OffsetDateTime;
+
+use crate::as_exchange::handle_as_req;
+use crate::config::{DomainUser, KerberosServer};
+use crate::error::KdcError;
+use crate::tgs_exchange::handle_tgs_req;
+
+fn find_user_credentials<'a>(
+    cname: &PrincipalName,
+    realm: &str,
+    kdc_config: &'a KerberosServer,
+) -> Result<&'a DomainUser, KdcError> {
+    let username = if cname.name_type.0.0 == [NT_PRINCIPAL] {
+        let cname = &cname
+            .name_string
+            .0
+            .first()
+            .ok_or(KdcError::ClientPrincipalUnknown(
+                "the incoming KDC request does not contain client principal name".to_owned(),
+            ))?
+            .0;
+        format!("{cname}@{realm}")
+    } else if cname.name_type.0.0 == [NT_ENTERPRISE] {
+        cname
+            .name_string
+            .0
+            .first()
+            .ok_or(KdcError::ClientPrincipalUnknown(
+                "the incoming KDC request does not contain client principal name".to_owned(),
+            ))?
+            .0
+            .to_string()
+    } else {
+        return Err(KdcError::InvalidCnameType(cname.name_type.0.0.clone()));
+    };
+
+    kdc_config
+        .users
+        .iter()
+        .find(|user| user.username.eq_ignore_ascii_case(&username))
+        .ok_or(KdcError::ClientPrincipalUnknown(format!(
+            "the requested client principal name ({username}) is not found in KDC database",
+        )))
+}
+
+/// Validates incoming `from` and `till` values of the [KdcReqBody].
+/// Returns `auth_time` and `end_time` for the issued ticket.
+///
+/// RFC: [Generation of KRB_AS_REP Message](https://www.rfc-editor.org/rfc/rfc4120#section-3.1.3).
+fn validate_request_from_and_till(
+    from: Option<&GeneralizedTimeAsn1>,
+    till: &GeneralizedTimeAsn1,
+    max_time_skew: u64,
+) -> Result<(OffsetDateTime, OffsetDateTime), KdcError> {
+    let now = OffsetDateTime::now_utc();
+    let max_time_skew = Duration::from_secs(max_time_skew);
+
+    let auth_time = if let Some(from) = from {
+        let from = OffsetDateTime::try_from(from.0.clone())
+            .map_err(|err| KdcError::NeverValid(format!("KdcReq::from time is not valid: {err}")))?;
+        // RFC (https://www.rfc-editor.org/rfc/rfc4120#section-3.1.3):
+        // > If the requested starttime is absent, indicates a time in the past,
+        // > or is within the window of acceptable clock skew for the KDC ...,
+        // > then the starttime of the ticket is set to the authentication server's current time.
+        if from < now + max_time_skew {
+            now
+        } else {
+            // RFC (https://www.rfc-editor.org/rfc/rfc4120#section-3.1.3):
+            // > If it indicates a time in the future beyond the acceptable clock skew, ..., then the error
+            // > KDC_ERR_CANNOT_POSTDATE is returned.
+            return Err(KdcError::CannotPostdate("KdcReq::from time is too far in the future"));
+        }
+    } else {
+        // RFC (https://www.rfc-editor.org/rfc/rfc4120#section-3.1.3):
+        // > If the requested starttime is absent, ..., then the starttime of the ticket is set to the authentication server's current time.
+        now
+    };
+
+    let till = OffsetDateTime::try_from(till.0.clone())
+        .map_err(|err| KdcError::NeverValid(format!("KdcReq::till time is not valid: {err}")))?;
+    let max_end_time = now + Duration::from_secs(60 * 60 /* 1 hour */);
+    // RFC (https://www.rfc-editor.org/rfc/rfc4120#section-3.1.3):
+    // > The expiration time of the ticket will be set to the earlier of the requested endtime and a time determined by local policy...
+    let end_time = till.min(max_end_time);
+
+    // RFC (https://www.rfc-editor.org/rfc/rfc4120#section-3.1.3):
+    // > If the requested expiration time minus the starttime (as determined above) is less than a site-determined minimum lifetime,
+    // > an error message with code KDC_ERR_NEVER_VALID is returned.
+    //
+    // We do not have a ticket minimum lifetime value configured, so we only check that the `end_time`` is after the `auth_time`.
+    if end_time < auth_time {
+        return Err(KdcError::NeverValid("end_time is earlier than auth_time".to_owned()));
+    }
+
+    Ok((auth_time, end_time))
+}
+
+/// Validates the service name of the incoming [KdcReqBody].
+fn validate_request_sname(sname: &PrincipalName, expected_snames: &[&str]) -> Result<(), KdcError> {
+    if sname.name_type.0.0 != [NT_SRV_INST] {
+        return Err(KdcError::InvalidSnameType(sname.name_type.0.0.clone()));
+    }
+
+    let mut sname = sname.name_string.0.0.iter().map(|name| name.to_string());
+
+    for expected_sname in expected_snames {
+        let service_name = sname
+            .next()
+            .ok_or_else(|| KdcError::InvalidSname(format!("'{expected_sname}' is not present in KDC request sname")))?;
+
+        if !service_name.eq_ignore_ascii_case(expected_sname) {
+            return Err(KdcError::InvalidSname(format!(
+                "KDC request sname ({service_name}) is not equal to '{expected_sname}'",
+            )));
+        }
+    }
+
+    if let Some(service_name) = sname.next() {
+        return Err(KdcError::InvalidSname(format!(
+            "unexpected {service_name} service name: KDC request sname has too many names inside",
+        )));
+    }
+
+    Ok(())
+}
+
+/// Handles [KdcProxyMessage] by mimicking the KDC.
+///
+/// The incoming [KdcProxyMessage] must contain either [AsReq] or [TgsReq] Kerberos message inside.
+/// This function is _almost_ infailible. Even when an error happens, it converts the error to [KrbError], then encodes it,
+/// and sends it back to the client. The only way this function can fail is when it fails to encode AS_REP/TGS_REP/KRB_ERROR.
+pub fn handle_kdc_proxy_message(
+    msg: KdcProxyMessage,
+    kdc_config: &KerberosServer,
+    hostname: &str,
+) -> Result<KdcProxyMessage, Asn1DerError> {
+    let KdcProxyMessage {
+        kerb_message,
+        target_domain,
+        dclocator_hint,
+    } = msg;
+    let raw_krb_message = &kerb_message
+        .0
+        .0
+        .as_slice()
+        .get(4..)
+        .ok_or_else(|| Asn1DerError::TruncatedData)?;
+
+    let reply_message = if let Ok(as_req) = picky_asn1_der::from_bytes::<AsReq>(raw_krb_message) {
+        match handle_as_req(&as_req, kdc_config) {
+            Ok(as_rep) => picky_asn1_der::to_vec(&as_rep)?,
+            Err(kdc_err) => picky_asn1_der::to_vec(&kdc_err.into_krb_error(&as_req.0.req_body, kdc_config))?,
+        }
+    } else if let Ok(tgs_req) = picky_asn1_der::from_bytes::<TgsReq>(raw_krb_message) {
+        match handle_tgs_req(&tgs_req, kdc_config, hostname) {
+            Ok(tgs_rep) => picky_asn1_der::to_vec(&tgs_rep)?,
+            Err(kdc_err) => picky_asn1_der::to_vec(&kdc_err.into_krb_error(&tgs_req.0.req_body, kdc_config))?,
+        }
+    } else {
+        picky_asn1_der::to_vec(&KdcError::invalid_raw_krb_message_error(kdc_config.realm.clone()))?
+    };
+
+    let len = reply_message.len();
+    let mut kerb_message = vec![0; len + 4];
+    kerb_message[0..4].copy_from_slice(&u32::try_from(len).expect("usize-to-u32").to_be_bytes());
+    kerb_message[4..].copy_from_slice(&reply_message);
+
+    Ok(KdcProxyMessage {
+        kerb_message: ExplicitContextTag0::from(OctetStringAsn1::from(kerb_message)),
+        target_domain,
+        dclocator_hint,
+    })
+}
