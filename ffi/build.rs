93068687297694cc62de5c35b535a8433b9ecdca
Marc-André Moreau <mamoreau@devolutions.net>
2026-02-04 11:51:59 -0500

    Embed Windows version info in sspi.dll (#594)

--- ffi/build.rs
+++ ffi/build.rs
@@ -1,11 +1,88 @@
 use std::path::PathBuf;
 use std::{env, fs};
 
+#[cfg(target_os = "windows")]
+mod win {
+    use std::env;
+    use std::fs::File;
+    use std::io::Write;
+    use std::path::PathBuf;
+
+    fn generate_version_rc() -> String {
+        let version = env::var("CARGO_PKG_VERSION").unwrap();
+        let version_parts: Vec<&str> = version.split('.').collect();
+
+        // Pad to 4 components (e.g., "0.18.7" -> "0.18.7.0")
+        let mut version_numbers = version_parts.clone();
+        while version_numbers.len() < 4 {
+            version_numbers.push("0");
+        }
+
+        let version_commas = version_numbers.join(",");
+        let version_dots = version_numbers.join(".");
+
+        let company_name = "Devolutions Inc.";
+        let file_description = "Devolutions SSPI";
+        let product_name = "Devolutions SSPI";
+        let original_filename = "sspi.dll";
+        let legal_copyright = "Copyright 2019-2026 Devolutions Inc.";
+
+        format!(
+            r#"#include <winver.h>
+
+VS_VERSION_INFO VERSIONINFO
+FILEVERSION {version_commas}
+PRODUCTVERSION {version_commas}
+FILEFLAGSMASK VS_FFI_FILEFLAGSMASK
+FILEFLAGS 0x0L
+FILEOS VOS_NT_WINDOWS32
+FILETYPE VFT_DLL
+FILESUBTYPE VFT2_UNKNOWN
+BEGIN
+    BLOCK "StringFileInfo"
+    BEGIN
+        BLOCK "040904B0"
+        BEGIN
+            VALUE "CompanyName", "{company_name}"
+            VALUE "FileDescription", "{file_description}"
+            VALUE "FileVersion", "{version_dots}"
+            VALUE "InternalName", "{original_filename}"
+            VALUE "LegalCopyright", "{legal_copyright}"
+            VALUE "OriginalFilename", "{original_filename}"
+            VALUE "ProductName", "{product_name}"
+            VALUE "ProductVersion", "{version_dots}"
+        END
+    END
+    BLOCK "VarFileInfo"
+    BEGIN
+        VALUE "Translation", 0x409, 1200
+    END
+END
+"#
+        )
+    }
+
+    pub(crate) fn embed_version_info() {
+        let out_dir = PathBuf::from(env::var("OUT_DIR").unwrap());
+        let version_rc_file = out_dir.join("version.rc");
+        let version_rc_data = generate_version_rc();
+
+        let mut file = File::create(&version_rc_file).expect("Failed to create version.rc file");
+        file.write_all(version_rc_data.as_bytes())
+            .expect("Failed to write version.rc file");
+
+        let _ = embed_resource::compile(&version_rc_file, embed_resource::NONE);
+    }
+}
+
 fn main() {
     let target_os = env::var("CARGO_CFG_TARGET_OS").unwrap();
     let is_running_tests = env::var("SSPI_RS_IS_RUNNING_TESTS").is_ok();
 
     if target_os == "windows" && !is_running_tests {
+        // Embed version information in the DLL
+        #[cfg(target_os = "windows")]
+        win::embed_version_info();
         // On Windows, we provide the linker with a .def file to rename exports.
         // This module definition file is used to rename some symbols
         // and avoid linkage conflicts with secur32.dll when building the library.
