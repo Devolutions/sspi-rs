07f280b93036b9f0fb41ade3ec587a2f1d92270f
Philippe Latulippe <philippelatulippe@users.noreply.github.com>
2026-06-01 08:15:30 -0400

    feat(ffi): generate dotnet bindings with csbindgen (#679)

---
+++ ffi/dotnet/Devolutions.Sspi/Sspi.DllImportResolver.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+namespace Devolutions.Sspi;
+
+public static unsafe partial class Sspi
+{
+    static Sspi()
+    {
+        NativeLibrary.SetDllImportResolver(typeof(Sspi).Assembly, DllImportResolver);  
+    }
+
+    static IntPtr DllImportResolver(string libraryName, Assembly assembly, DllImportSearchPath? searchPath)
+    {
+        if (RuntimeInformation.IsOSPlatform(OSPlatform.Create("iOS")))
+        {
+            return NativeLibrary.Load("Frameworks/lib" + libraryName + ".framework/lib" + libraryName, assembly, searchPath);
+        }
+        else
+        {
+            return NativeLibrary.Load(libraryName, assembly, searchPath);
+        }
+    }
+}
---
+++ ffi/dotnet/PackageTests/UnitTests.cs
@@ -0,0 +1,60 @@
+﻿using System.Runtime.InteropServices;
+using Devolutions.Sspi;
+
+namespace PackageTests;
+
+public class UnitTests
+{
+    [Fact]
+    public void DpapiUnprotectSecret_Calling_ReturnsResultValue()
+    {
+        unsafe
+        {
+            byte[] blob = "some encrypted stuff, a terrible secret"u8.ToArray();
+
+            IntPtr serverUnmanaged = Marshal.StringToCoTaskMemUTF8("hostname");
+            IntPtr usernameUnmanaged = Marshal.StringToCoTaskMemUTF8("user");
+            IntPtr passwordUnmanaged = Marshal.StringToCoTaskMemUTF8("password");
+            IntPtr computerNameUnmanaged = Marshal.StringToCoTaskMemUTF8(Environment.MachineName);
+
+            fixed (byte* pBlob = blob)
+            {
+
+                byte* secret;
+                uint secretLen;
+                uint returnCode = Sspi.DpapiUnprotectSecret(
+                    pBlob,
+                    (uint)blob.Length,
+                    (byte*)serverUnmanaged,
+                    (byte*)usernameUnmanaged,
+                    (byte*)passwordUnmanaged,
+                    (byte*)computerNameUnmanaged,
+                    null,
+                    null,
+                    &secret,
+                    &secretLen);
+                
+                Marshal.ZeroFreeCoTaskMemUTF8(serverUnmanaged);
+                Marshal.ZeroFreeCoTaskMemUTF8(usernameUnmanaged);
+                Marshal.ZeroFreeCoTaskMemUTF8(passwordUnmanaged);
+                Marshal.ZeroFreeCoTaskMemUTF8(computerNameUnmanaged);
+                
+                Assert.True(returnCode is 0 or >= 0x8000000 and < 0x90000000);
+            }
+        }
+    }
+
+    [Fact]
+    public void SspiInitSecurityInterface_Calling_HasPlausibleVersion()
+    {
+        
+        unsafe
+        {
+            SecurityFunctionTableW* sspiTablePtr = Sspi.InitSecurityInterfaceW();
+
+            SecurityFunctionTableW sspiTable = *sspiTablePtr;
+            
+            Assert.InRange<uint>(sspiTable.dwVersion, 0, 1000);
+        }
+    }
+}
